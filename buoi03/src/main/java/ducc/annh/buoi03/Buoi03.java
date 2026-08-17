67d397eff0e9e1eb897689afc069d00af68863a5
Đào Đức Anh <xihaotungcua@gmail.com>
2026-08-17 22:54:17 +0700

    My test

---
+++ buoi03/src/main/java/ducc/annh/buoi03/Buoi03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ducc.annh.buoi03;
+
+/**
+ *
+ * @author xihao
+ */
+public class Buoi03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
